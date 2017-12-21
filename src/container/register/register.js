37e9220ef4f3c1bc5b9a7fdc60b2b10a21b91ab8
flowOnWandering <sure075846@gmail.com>
2017-12-21 18:46:26 +0800

    add

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import Logo from '../../component/logo/logo'
+import {List, InputItem, Radio, WhiteSpace, Button} from "antd-mobile";
+import { connect } from "react-redux";
+import { Redirect } from 'react-router-dom';
+import { regisger } from "../redux/user.redux";
 
+@connect(
+	state=>state.user,
+	{regisger}
+)
 class Register extends React.Component{
-	render(){
-		return <h2>注册页</h2>
+	constructor(props){
+		super(props)
+		this.state = {
+			user:'',
+			pwd:'',
+			repeatpwd:'',
+			type:'genius'
+		}
+		this.handleRegister = this.handleRegister.bind(this)
 	}
+	handleChange(key,val){
+		this.setState({
+			[key]:val
+		})
+	}
+	handleRegister(){
+		this.props.regisger(this.state)
+	}
+	render(){
+		const RadioItem = Radio.RadioItem
+		return(
+			<div>
+				{this.props.redirectTo?<Redirect to={this.props.redirectTo}/>:null}
+				<Logo></Logo>
+				<List>
+					{this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
+					<InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
+					<WhiteSpace/>
+					<InputItem onChange={v => this.handleChange('pwd', v)} type="password">密码</InputItem>
+					<WhiteSpace />
+					<InputItem onChange={v => this.handleChange('repeatpwd', v)} type="password">确认密码</InputItem>
+					<WhiteSpace />
+					<RadioItem checked={this.state.type ==='genius'} onChange={()=>this.handleChange('type','genius')}>牛人</RadioItem>
+					<WhiteSpace />
+					<RadioItem checked={this.state.type === 'boss'} onChange={() => this.handleChange('type', 'boss')}>BOSS</RadioItem>
+					<WhiteSpace />
+					<Button type='primary' onClick={this.handleRegister}>注册</Button>
+				</List>
+			</div>
+		)
+	}
+
 }
+
+
+
 
 export default Register
