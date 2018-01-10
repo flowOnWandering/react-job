9ce562ecb547c2949ff7ef1e34b3c1a3772639c9
flowOnWandering <sure075846@gmail.com>
2018-01-10 11:57:55 +0800

    add

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,56 +1,53 @@
 import React from 'react'
-import Logo from "../../component/logo/logo";
-import { List, InputItem, WingBlank, WhiteSpace, Button } from "antd-mobile";
-import { connect } from "react-redux";
-import { login } from "../../redux/user.redux.js";
+import Logo from '../../component/logo/logo'
+import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-
+import { login } from '../../redux/user.redux'
+import imoocFrom from '../../component/imooc-form/imooc-form.js'
 @connect(
-	state=>state.user,
-	{login}
+	state => state.user,
+	{ login }
 )
-
-class Login extends React.Component{
-	constructor(props){
+@imoocFrom
+class Login extends React.Component {
+	constructor(props) {
 		super(props)
-		this.state = {
-			user:'',
-			pwd:''
-		}
 		this.register = this.register.bind(this)
 		this.handleLogin = this.handleLogin.bind(this)
 	}
-	register(){
+	register() {
 		this.props.history.push('/register')
 	}
-	handleChange(key,val){
-		this.setState({
-			[key]:val
-		})
+	handleLogin() {
+		this.props.login(this.props.state)
 	}
-	handleLogin(){
-		this.props.login(this.state)
-	}
-
-	render(){
+	render() {
 		return (
 			<div>
-				{this.props.redirectTo? <Redirect to={this.props.redirectTo}/>:null}
-				{/*从redux传过来的redirectTo*/}
+
+				{(this.props.redirectTo && this.props.redirectTo != '/login') ? <Redirect to={this.props.redirectTo} /> : null}
 				<Logo></Logo>
 				<WingBlank>
 					<List>
-						{this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
-						<InputItem onChange={v => this.handleChange('user', v)}>用户</InputItem>
-						<WhiteSpace/>
-						<InputItem onChange={v => this.handleChange('pwd', v)} type='password'>密码</InputItem>
+						{this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : null}
+						<InputItem
+							onChange={v => this.props.handleChange('user', v)}
+
+						>用户</InputItem>
+						<WhiteSpace />
+						<InputItem
+							onChange={v => this.props.handleChange('pwd', v)}
+							type='password'
+						>密码</InputItem>
 					</List>
-					<WhiteSpace/>
-					<Button onClick={this.handleLogin} type="primary">登陆</Button>
 					<WhiteSpace />
-					<Button onClick={this.register} type="primary">注册</Button>
-					
+					<Button onClick={this.handleLogin} type='primary'>登录</Button>
+					<WhiteSpace />
+					<Button onClick={this.register} type='primary'>注册</Button>
 				</WingBlank>
+
+
 			</div>
 		)
 	}
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,60 +1,69 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
-import {List, InputItem, Radio, WhiteSpace, Button} from "antd-mobile";
-import { connect } from "react-redux";
-import { Redirect } from 'react-router-dom';
-import { regisger } from "../../redux/user.redux";
-
+import { List, InputItem, Radio, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { regisger } from '../../redux/user.redux'
+import imoocForm from '../../component/imooc-form/imooc-form'
 @connect(
-	state=>state.user,
-	{regisger}
+	state => state.user,
+	{ regisger }
 )
-class Register extends React.Component{
-	constructor(props){
+@imoocForm
+class Register extends React.Component {
+	constructor(props) {
 		super(props)
-		this.state = {
-			user:'',
-			pwd:'',
-			repeatpwd:'',
-			type:'genius'
-		}
 		this.handleRegister = this.handleRegister.bind(this)
 	}
-	handleChange(key,val){
-		this.setState({
-			[key]:val
-		})
+	componentDidMount() {
+		this.props.handleChange('type', 'genius')
 	}
-	handleRegister(){
-		this.props.regisger(this.state)
+	handleRegister() {
+		this.props.regisger(this.props.state)
 	}
-	render(){
+	render() {
 		const RadioItem = Radio.RadioItem
-		return(
+		return (
 			<div>
-				{this.props.redirectTo?<Redirect to={this.props.redirectTo}/>:null}
+				{this.props.redirectTo ? <Redirect to={this.props.redirectTo} /> : null}
 				<Logo></Logo>
 				<List>
-					{this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
-					<InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
-					<WhiteSpace/>
-					<InputItem onChange={v => this.handleChange('pwd', v)} type="password">密码</InputItem>
+					{this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : null}
+					<InputItem
+						onChange={v => this.props.handleChange('user', v)}
+					>用户名</InputItem>
 					<WhiteSpace />
-					<InputItem onChange={v => this.handleChange('repeatpwd', v)} type="password">确认密码</InputItem>
+					<InputItem
+						type='password'
+						onChange={v => this.props.handleChange('pwd', v)}
+					>密码</InputItem>
 					<WhiteSpace />
-					<RadioItem checked={this.state.type ==='genius'} onChange={()=>this.handleChange('type','genius')}>牛人</RadioItem>
+					<InputItem
+						type='password'
+						onChange={v => this.props.handleChange('repeatpwd', v)}
+					>确认密码</InputItem>
 					<WhiteSpace />
-					<RadioItem checked={this.state.type === 'boss'} onChange={() => this.handleChange('type', 'boss')}>BOSS</RadioItem>
+					<RadioItem
+						checked={this.props.state.type == 'genius'}
+						onChange={() => this.props.handleChange('type', 'genius')}
+					>
+						牛人
+					</RadioItem>
+					<RadioItem
+						checked={this.props.state.type == 'boss'}
+						onChange={() => this.props.handleChange('type', 'boss')}
+					>
+						BOSS
+					</RadioItem>
 					<WhiteSpace />
-					<Button type='primary' onClick={this.handleRegister}>注册</Button>
+					<Button type='primary' onClick={this.handleRegister}>注册 </Button>
 				</List>
+
+
 			</div>
+
 		)
 	}
-
 }
-
-
-
 
 export default Register
